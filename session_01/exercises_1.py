1dbe3d541290df97b84cb8801d0f0b167c16bf0a
Replit user <>
2023-06-06 09:21:26 +0000

    session_01

--- session_01/exercises_1.py
+++ session_01/exercises_1.py
@@ -1,4 +1,4 @@
-# Session 1 Exercises
+  # Session 1 Exercises
 
 # Please have a look at the best_practices.md file beforehand.
 # Don't forget to comment out any piece of code that you dont want to be executed when running the file.
@@ -11,15 +11,16 @@
 
 ## Section A
 # 1. Write code that prints ‘Hello world’.
+# print("Hello World")
 
 
 
 # 2. Print the numbers 1 to 5 on a single line.
-
+print(1,2,3,4,5)
 
 
 # 3. Write a script where ‘Hello’ and ‘World’ are printed on two separate lines.
-
+print("Hello\nWorld")
 
 
 # 4. Write a script that prints a list of names, tabbed on separate lines, e.g.
@@ -28,7 +29,7 @@
 #             Bob
 #             Charlie
 #     ```
-
+print("My List of Names:\n\tAlice\n\tBob\n\tCharlie")
 
 
 
@@ -36,23 +37,26 @@
 
 ## Section B
 # 1. Write code that prints the value of 2 + 2.
-
+print(2+2)
 
 
 # 2. Write code that prints the value of 5.7 subtracted from 3.4.
-
+print(3.4-5.7)
 
 
 # 3. Write code that prints the value of 8 multiplied by 7.
-
+print(8*7)
 
 
 # 4. Write code that prints the value of 144 divided by 12.
-
+print(144/12)
 
 
 # 5. Write code that prints the value of the remainder of 67 divided by 12.
-
+remainder = 67%12
+print(remainder)
 
 
 # 6. Write code that finds the value of 20 from `4 - 2 * 6 / 3 * 5`. Hint: you might need brackets.
+ans = (4-2)*6/3*5
+print(ans)
