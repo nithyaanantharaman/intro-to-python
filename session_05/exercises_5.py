0c6fe243c0fcf9924c65125ee850ff5ca23eac05
Replit user <>
2023-07-11 10:53:51 +0000

    session06

--- session_05/exercises_5.py
+++ session_05/exercises_5.py
@@ -2,43 +2,88 @@
 
 ## Section A
 # 1. Print 10 random numbers.
-
-
+#import random
+#for number in range(10):
+ #   print(random.randint(1, 10))
 
 # 2. Keep asking the user to enter a number until they enter the number 7, then print "Wow lucky number 7!".
 #     - Rewrite so that the number being guessed is a random value from 1 to 10 instead of number 7 .
-
-
+#import random
+#guess = None
+#while guess != 7:
+#    guess = int(input("Guess the computer's number:\n"))
+#print("Wow! Lucky number 7!")
 
 # 3. The area of a rectangle is width multiplied by height. Ask the user to enter a width and height in cm, then print the area to the whole square metre. 
 #   E.g. 240cm x 80cm = 19200cm2 = 2m2.
-
-
+#import math
+#width = int(input("What is the width of your rectangle:\n"))
+#height = int(input("What is the height of your rectangle:\n"))
+#area_in_cm = width * height
+#area_in_m2 = math.ceil(area_in_cm/10000)
+#print("Your rectangle is " + str(area_in_m2) + ' metre squared.')
 
 # 4. Ask the user for a password, if they enter the password "qwerty123", print "You have successfully logged in". 
 #   If they get it wrong, print "Password failure" and then ask them to enter it again. 
 #   Only allow them to enter the password wrong 3 times before printing "System Locked!".
+#user_input = input("What is your password?\n")
+#password = "qwerty123"
+#user_guesses = 0
+#while user_guesses < 2:
+  #  if user_input == password:
+     #   print("You have successfully logged in.")
+        #break
+    #else:
+     #   print("PASSWORD FAILURE\n")
+      #  user_input = input("What is your password?\n")
+       # user_guesses += 1
 
-
-
+#if user_guesses == 2:
+  #print("PASSWORD FAILURE\n")
+ # print("System Failure - You have entered 3 incorrect passwords.")
 # 5. Add up all the numbers from 1 to 500 and print the answer.
 
+#total = 0
+
+#for x in range(1, 501):
+   # total += x
+
+#print(total)
 
 
 # 6. Create a blank list. Ask the user to input 10 numbers (one should be the number 99) into the list. 
 #   Find the index at which the user entered the number 99.
+#num_list = []
+#numbers = ""
+#i = 0
+#x = 0 
+#print("Pick 10 numbers, one of those numbers must be 99")
 
+#while i < 10:
+ # numbers = int(input("Pick a number:\n"))
+ # num_list.append(numbers)
+ # i += 1 
 
-
+#while x < len(num_list):
+ # if num_list[x] == 99:
+  #  print("Number 99 is at index " + str(x))
+  #x += 1
+  
 # 7. Print a multiplication table for the number 18 up to 15. E.g.
 #     1 x 18 = 18
 #     2 x 18 = 36
-
+#i=0
+#while i < 15:
+ # print(i*18)
+  #i += 1
 
 
 # 8. Print the numbers 1 to 100 (including the number 100) using a while loop.
 
-
+#i=0
+#while i < 101:
+ #   print(i)
+ #   i += 1
 
 # 9. Rewrite question B8 from session 3 using a while loop
 #     - A school has following rules for their grading system:
--- session_06/exercises_6.py
+++ session_06/exercises_6.py
@@ -2,36 +2,99 @@
 
 ## Section A
 # 1. Create the following dictionary for an apple: Type = "Bramley", Price = 0.39, Colour = "Green".
-
-
+#apple = {
+ #   "Type": "Bramley",
+  #  "Price": "0.39",
+   # "Colour": "Green"
+#}
+#print (apple)
 
 # 2. Add the best before date to the dictionary - print the dictionary.
-
-
+#apple = {
+   # "Type": "Bramley",
+   # "Price": "0.39",
+   # "Colour": "Green"
+#}
+#apple ["best before date"] = "11.07.2023"
+#print (apple)
 
 # 3. Change the price to 0.41 - print the dictionary.
-
-
+#apple = {
+ #   "Type": "Bramley",
+  #  "Price": "0.39",
+   # "Colour": "Green"
+#}
+#apple ["Price"] ="0.41"
+#print (apple)
 
 # 4. Set the apple to be on offer using a Boolean - print the dictionary.
-
-
+#apple = {
+ #   "Type": "Bramley",
+ #   "Price": "0.39",
+ #   "Colour": "Green"
+#}
+#apple ["offer"] = True
+#print (apple)
 
 # 5. The offer has now expired, remove the key/value from the dictionary - print the dictionary.
-
-
+#del (apple["offer"])
+#print (apple)
 # <---------------------------------------------------------------------------------------------->
 
 ## Section B
 # 1. Ask the user to enter a persons name, if they enter a name, ask for the persons age. Store this information in a dictionary inside a list. 
 #   Continue to ask for names until no name is given. Then print out all of the names and ages collected.
-
+#contacts = []
+#fname = None
+ 
+#while fname != "":
+ #   fname = input("What is your first name? ")
+  #  if fname:
+   #     age = int(input("What is your age? "))
+    #    contacts.append(
+     #       {
+      #          "fname": fname,
+       #         "age": age
+        #    }
+        #)
+#print(contacts)
 
 
 # 2. Create a restaurants menu with 5 items. Store this information in a dictionary inside a list. 
 #   Each item in the menu should have the name of the item, the price and if it's vegetarian friendly (make at least one vegetarian friendly dish). 
 #   Print out the entire menu. Print out the name of the vegetarian option(s).
+#menu = [
+ #     {
+  #      "name": "tofu fritters",
+   #     "price": 5.99,
+    #    "vegetarian_friendly": True
+     # },
+     # {
+      #  "name": "black bean curry",
+       # "price": 6.25,
+       # "vegetarian_friendly": True
+     # },
+     # {
+      #  "name": "shepherds pie",
+       # "price": 9.99,
+        #"vegetarian_friendly": False
+      #},
+      #{
+       # "name": "cheese and potato pie",
+        #"price": 5.00,
+        #"vegetarian_friendly": True
+     # },
+     # {
+      #  "name": "fries",
+       # "price": 0.99,
+        #"vegetarian_friendly": True
+      #}
+    #]
 
+# print(menu)
+#for dish in menu:
+ # if dish["vegetarian_friendly"]:
+  #  print(dish["name"])
 
 
 # 3. The beetle game is a dice game where depending on what you roll is how much of the beetle you can draw.
